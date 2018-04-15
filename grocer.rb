502925d386fd4cfab394dc85c6713959b61b5518
Michelle Blevins <mjblevins7@gmail.com>
2018-04-15 20:03:10 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -35,9 +35,9 @@ def apply_clearance(cart)
   cart.each do |item, item_hash|
     if item_hash[:clearance] == true
       item_hash[:price] *= 0.8
-      (item_hash[:price]).round(2)
-    end
 
+    end
+    item_hash[:price].round(2)
   end
 cart
 
