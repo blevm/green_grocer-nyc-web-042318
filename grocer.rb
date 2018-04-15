89f50bb5bae1589b2bdf8be72a4860618a56a3a8
Michelle Blevins <mjblevins7@gmail.com>
2018-04-15 18:52:26 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -4,22 +4,16 @@ def consolidate_cart(cart)
 
     cart.each_with_object(Hash.new(0)) do |cart_list, final_list|
       cart_list.each do |item_key, item_details_hash|
-        final_list[item_key] = {}
-        final_list[item_key] = item_details_hash
-        final_list[item_key][:count] = 0
-        item = item_key
-
-        cart.each do |cart_list|
-          cart_list.each do |item_key, item_details_hash|
-            if item_key == item
-              final_list[item_key][:count] += 1
-            end
-          end
+        if final_list[item_key]
+          final_list[item_key][:count] = 0
+        else
+          final_list[item_key] = item_details_hash
+          final_list[item_key][:count] += 1
         end
 
       end
     end
-    cart
+    
 end
 
 def apply_coupons(cart, coupons)
